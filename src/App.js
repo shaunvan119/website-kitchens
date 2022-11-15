bbf0c283bf1e08b0976339a9a2db2c538a5cf9f0
shaunvan119 <shaunvan@live.com.au>
2022-11-15 13:35:00 +1100

    fixing facebook chat errors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import Header from "./containers/Header";
 import Main from "./containers/Main";
 import Footer from "./containers/Footer";
 import BottomMain from "./containers/BottomMain";
-
+import { useEffect} from 'react';
 
 
 function App() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall();
+  }, [])
   return (
     <div className="App">
    
